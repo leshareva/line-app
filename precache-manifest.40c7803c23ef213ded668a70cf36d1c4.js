47a4461cc7312190ea938f27521e38edb02811b8
leshareva <leshareva.box@gmail.com>
2020-05-07 11:24:15 +0700

    Updates

---
+++ precache-manifest.40c7803c23ef213ded668a70cf36d1c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63fc01f5dde1f2f498919e479532c7c",
+    "url": "/line-app/index.html"
+  },
+  {
+    "revision": "dd8f10d1c51f429b0b11",
+    "url": "/line-app/static/css/2.9e627a5a.chunk.css"
+  },
+  {
+    "revision": "11f9b6b7bd8fc35f5042",
+    "url": "/line-app/static/css/main.9c4a77be.chunk.css"
+  },
+  {
+    "revision": "dd8f10d1c51f429b0b11",
+    "url": "/line-app/static/js/2.366d0c6e.chunk.js"
+  },
+  {
+    "revision": "11f9b6b7bd8fc35f5042",
+    "url": "/line-app/static/js/main.43c4e6c9.chunk.js"
+  },
+  {
+    "revision": "4273971a66f133895e50",
+    "url": "/line-app/static/js/runtime-main.ae810114.js"
+  },
+  {
+    "revision": "75e96a0e8069bd9f11b6360293f311d4",
+    "url": "/line-app/static/media/splash.75e96a0e.GIF"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/line-app/precache-manifest.2e226d637b3791aea93c76985a5d74d8.js"
+  "/line-app/precache-manifest.40c7803c23ef213ded668a70cf36d1c4.js"
 );
 
 self.addEventListener('message', (event) => {
