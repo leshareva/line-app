4e107ce0d3c75d4bb8eaa2a4489831f0c491bf70
leshareva <leshareva.box@gmail.com>
2020-05-22 20:36:34 +0700

    Updates

---
+++ precache-manifest.5b86606ab1211f4fc5d2f7164f770465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d806aaa660ba50dddbebefbc7178a4",
+    "url": "/line-app/index.html"
+  },
+  {
+    "revision": "03466c14d0345db8e668",
+    "url": "/line-app/static/css/2.9e627a5a.chunk.css"
+  },
+  {
+    "revision": "592ff1126fb2541fb25e",
+    "url": "/line-app/static/css/main.320ed416.chunk.css"
+  },
+  {
+    "revision": "03466c14d0345db8e668",
+    "url": "/line-app/static/js/2.d21adf5c.chunk.js"
+  },
+  {
+    "revision": "592ff1126fb2541fb25e",
+    "url": "/line-app/static/js/main.476228fd.chunk.js"
+  },
+  {
+    "revision": "4273971a66f133895e50",
+    "url": "/line-app/static/js/runtime-main.ae810114.js"
+  },
+  {
+    "revision": "75e96a0e8069bd9f11b6360293f311d4",
+    "url": "/line-app/static/media/splash.75e96a0e.GIF"
+  }
+]);
